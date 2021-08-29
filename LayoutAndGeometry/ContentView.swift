3337106f6f5bd2efb709b083042b80058e948b54
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-08-29 23:29:17 +0100

    Alignment and alignment guides

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -9,13 +9,88 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Example4View()
+        Example10View()
     }
 }
 
 struct ExampleView: View {
     var body: some View {
         Text("Hello, world!")
+    }
+}
+
+struct Example10View: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(0 ..< 10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in
+                        CGFloat(position) * -10
+                    }
+            }
+        }
+        .background(Color.red)
+        .frame(width: 400, height: 400)
+        .background(Color.blue)
+    }
+}
+
+struct Example9View: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+                .alignmentGuide(.leading) { d in d[.trailing] }
+            Text("This is a longer line of text")
+        }
+        .background(Color.red)
+        .frame(width: 400, height: 400)
+        .background(Color.blue)
+    }
+}
+
+struct Example8View: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+                .alignmentGuide(.leading) { d in d[.leading] }
+            Text("This is a longer line of text")
+        }
+        .background(Color.red)
+        .frame(width: 400, height: 400)
+        .background(Color.blue)
+    }
+}
+
+struct Example7View: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+            Text("This is a longer line of text")
+        }
+        .background(Color.red)
+        .frame(width: 400, height: 400)
+        .background(Color.blue)
+    }
+}
+
+struct Example6View: View {
+    var body: some View {
+        HStack(alignment: .lastTextBaseline) {
+            Text("Live")
+                .font(.caption)
+            Text("long")
+            Text("and")
+                .font(.title)
+            Text("prosper")
+                .font(.largeTitle)
+        }
+    }
+}
+
+struct Example5View: View {
+    var body: some View {
+        Text("Live long and prosper")
+            .frame(width: 300, height: 300, alignment: .topLeading)
     }
 }
 
